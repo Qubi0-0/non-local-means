5392bf2b14f3c1a4c038511839f534965ef17e52
Qubi0-0 <kuba1102@gmail.com>
2022-11-18 15:15:31 +0100

    declassed get_img return

--- main.py
+++ main.py
@@ -12,22 +12,23 @@ class Denoising:
     def get_img(self,file_path):
         self.img = cv.imread(file_path)
         cv.imshow('original',self.img)
+        return self.img
 
-    def addNoise(self, noiseType, p = 0.001, mean = 0,  sigma = 0.3):
+    def addNoise(self, img,noiseType, p = 0.001, mean = 0,  sigma = 0.3):
         ''' 
         This function takes an self.img and returns an self.img that has been noised with the given input parameters.
         p - Probability threshold of salt and pepper noise.
         noisetype - 
         '''
         if noiseType.upper() == 'GAUSSIAN':
-            sigma *= 255 #Since the self.img itself is not normalized
-            noise = np.zeros_like(self.img)
+            sigma *= 255 #Since the img itself is not normalized
+            noise = np.zeros_like(img)
             noise = cv.randn(noise, mean, sigma)
-            output = cv.add(self.img, noise) #generate and add gaussian noise
+            output = cv.add(img, noise) #generate and add gaussian noise
             return output
         elif noiseType.upper() == 'SALTNPEPPER':
-            output = self.img.copy()
-            noise = np.random.rand(self.img.shape[0], self.img.shape[1])
+            output = img.copy()
+            noise = np.random.rand(img.shape[0], img.shape[1])
             output[noise < p] = 0
             output[noise > (1-p)] = 255
             return output
@@ -38,8 +39,8 @@ class Denoising:
 if __name__ == '__main__':
 
     denoiser = Denoising()
-    denoiser.get_img("photos/lena.png")
-    result = denoiser.addNoise("Gaussian", mean= 2, sigma= 4)
+    lena_img = denoiser.get_img("photos/lena.png")
+    result = denoiser.addNoise(lena_img,"Gaussian", mean= 2, sigma= 4)
     cv.imshow('test',result)
 
     cv.waitKey(0) 
