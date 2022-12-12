da88531095153a08d7dfae3291f5940e4abf0a8a
Qubi0-0 <kuba1102@gmail.com>
2022-12-12 19:22:50 +0100

    algorithm works

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-import matplotlib 
+import matplotlib.pyplot as plt
 from numba import jit
 from time import sleep
 from tqdm import tqdm
@@ -33,27 +33,27 @@ def add_noise(img,noiseType, p = 0.001, mean = 0,  sigma = 0.3, show = False):
     return result
 
 
-
+def PSNR(original, noisy, peak=100):
+    mse = np.mean((original-noisy)**2)
+    return 10*np.log10(peak*peak/mse)
 
 
 @jit(nopython=True) 
-def non_local_means_computing(input, bordered_img, neighbour_window_size, patch_window_size, sigma, layers):
+def non_local_means_computing(input, bordered_img, neighbour_window_size, patch_window_size, sigma,h ,layers):
     '''Performs the non-local-means algorithm given a input img.'''
 
     neighbour_width = neighbour_window_size//2
     patch_width = patch_window_size//2
     img = input.copy()
-
-    max_progress = (img.shape[1]*img.shape[0]*(neighbour_window_size - patch_window_size)**2)
-    h = 0.4*sigma
+    max_progress = (img.shape[1]*img.shape[0]*(neighbour_window_size - patch_window_size)**2)*layers
     result = bordered_img.copy()
 
-
+    
 
     # with tqdm(total=max_progress) as progress_bar:
     for layer in range(layers):
         progress = 0
-        for img_x in  range(neighbour_width , neighbour_width + img.shape[0]):
+        for img_x in range(neighbour_width , neighbour_width + img.shape[0]):
             for img_y in range(neighbour_width, neighbour_width + img.shape[1]):
                 win_x = img_x - neighbour_width
                 win_y = img_y - neighbour_width
@@ -63,7 +63,6 @@ def non_local_means_computing(input, bordered_img, neighbour_window_size, patch_
 
                 pix_val = 0
                 weight_sum = 0
-
                 for patch_x in range(win_x,win_x + neighbour_window_size - patch_window_size):
                     for patch_y in range(win_y,win_y + neighbour_window_size - patch_window_size):
 
@@ -71,18 +70,18 @@ def non_local_means_computing(input, bordered_img, neighbour_window_size, patch_
                                                     patch_y:patch_y+patch_window_size + 1,layer]
 
 
-                        euclidean_dist = np.sqrt(np.sum(np.square(patch_window - neighbour_window)))
-                        weight = np.exp(-euclidean_dist/h**2)
-                        weight_sum += weight 
-                
+                        euclidean_dist = (np.sum(np.square(patch_window - neighbour_window)))
+                        # euclidean_dist = euclidean_dist/3*(patch_window_size**2)                        
+                        weight = np.exp(-max(euclidean_dist -2*sigma**2, 0.0)/h**2)
+                        weight_sum += weight                
                         pix_val += weight*bordered_img[patch_x + patch_width,
                                                     patch_y + patch_width,layer]
                         # progress_bar.update(1)
                         progress += 1
                         percent_completed = progress*100/max_progress
                         if percent_completed % 10 == 0:
-                            print('Completed : ', percent_completed,'precent of layer number: ',layer+1)
-
+                            print('Completed in: ', percent_completed,'precent of layer number: ',layer+1)
+                
                 pix_val /= weight_sum
 
                 result[img_x,img_y,layer] = pix_val
@@ -92,7 +91,7 @@ def non_local_means_computing(input, bordered_img, neighbour_window_size, patch_
 
 
 
-def non_local_means_initiate(input, neighbour_window_size, patch_window_size,sigma):
+def non_local_means_initiate(input, neighbour_window_size, patch_window_size,h,sigma):
     # reflects borders to allow computing on the edges
     bordered_img = cv.copyMakeBorder(input, neighbour_window_size//2, neighbour_window_size//2, neighbour_window_size//2, neighbour_window_size//2, cv.BORDER_REFLECT)
     if len(input.shape) > 2:
@@ -103,7 +102,7 @@ def non_local_means_initiate(input, neighbour_window_size, patch_window_size,sig
         input = np.expand_dims(input, axis=2)
         bordered_img = np.expand_dims(bordered_img, axis=2)
 
-    result = non_local_means_computing(input, bordered_img, neighbour_window_size, patch_window_size,sigma,layers)
+    result = non_local_means_computing(input, bordered_img, neighbour_window_size, patch_window_size,sigma,h,layers)
     cv.imshow("Final", result)
 
 
@@ -111,8 +110,8 @@ if __name__ == '__main__':
 
     lena_img = get_img("photos/lena.png")
     lena_img = cv.cvtColor(lena_img, cv.COLOR_BGR2GRAY) 
-    result = add_noise(lena_img,"SALTNPEPPER", p= 0.001, mean= 0, sigma= 0.3)
-    non_local_means_initiate(result,neighbour_window_size= 20,patch_window_size= 6,sigma= 10)
+    result = add_noise(lena_img,"SALTNPEPPER", p= 0.001, mean= 0, sigma= 0.3,show = True)
+    non_local_means_initiate(result,neighbour_window_size= 20,patch_window_size= 6,h = 19,sigma= 36)
 
     cv.waitKey(0) 
     cv.destroyAllWindows()
