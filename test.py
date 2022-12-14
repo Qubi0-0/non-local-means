81043ee46d0d8f3d612cb202795fb7efcec9539d
Qubi0-0 <kuba1102@gmail.com>
2022-12-14 12:07:04 +0100

    img set changed

--- test.py
+++ test.py
@@ -11,9 +11,12 @@ def get_img(file_path):
 
 
 
-img = get_img("photos/lena.png")
+# img = get_img("photos/lena.png")
 
-image = cv.copyMakeBorder(img, 1000, 1000, 50, 50, cv.BORDER_REFLECT)
-cv.imshow('zdjecie',image)
-cv.waitKey(0)
-cv.destroyAllWindows()
+# image = cv.copyMakeBorder(img, 1000, 1000, 50, 50, cv.BORDER_REFLECT)
+# cv.imshow('zdjecie',image)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+for i in range(3):
+    print(i)
